81e653ee97a1bdc3a9c4edbae12a0f8ba25732e9
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2021-03-30 14:34:16 +0800

    This Push is broken
    If something is wrong revert this, I want to give a go fixing it though before I straight up revert

---
+++ Audio Bites/Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,163 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void OptionConsistency::.ctor()
+extern void OptionConsistency__ctor_m931190602B62B8B1733C892DAFCAA542A6E4BD0E (void);
+// 0x00000002 System.Void NewBehaviourScript::Start()
+extern void NewBehaviourScript_Start_m783F84A617DADC4574B0BF1524481E6B96C65661 (void);
+// 0x00000003 System.Void NewBehaviourScript::Update()
+extern void NewBehaviourScript_Update_m411C4D5C2D993FD70092FDA0FE2AC4786F8AC001 (void);
+// 0x00000004 System.Void NewBehaviourScript::.ctor()
+extern void NewBehaviourScript__ctor_m437970EA37D66BDF32972F4CC0F65B95E5961FAA (void);
+// 0x00000005 System.Void ChangeCheckBox::Update()
+extern void ChangeCheckBox_Update_m3FFF2668E216715A6B9F25780D2C97757941FE0C (void);
+// 0x00000006 System.Void ChangeCheckBox::.ctor()
+extern void ChangeCheckBox__ctor_m8C1B96BC249441C39F6EED5A085A78F9CC8EB76B (void);
+// 0x00000007 System.Void ChangeSwitchOnOff::Update()
+extern void ChangeSwitchOnOff_Update_mC5D7F1D03BE42DFCA6EA95809AE8148E0023DDAE (void);
+// 0x00000008 System.Void ChangeSwitchOnOff::.ctor()
+extern void ChangeSwitchOnOff__ctor_m3FFDD1A009F7820404AC21FDECB605A7A427023B (void);
+// 0x00000009 System.Void ClickManager::Awake()
+extern void ClickManager_Awake_m8C1F7C71D1AA3E1CC537E6C385BC3DA4B88D046C (void);
+// 0x0000000A System.Void ClickManager::Update()
+extern void ClickManager_Update_mEE9B6D5B06132A499849992D38ED889DCA287317 (void);
+// 0x0000000B System.Void ClickManager::SceneChange(System.String)
+extern void ClickManager_SceneChange_m5DEACF16589656AED9DA6A240B68C5BE499B0FE8 (void);
+// 0x0000000C System.Void ClickManager::.ctor()
+extern void ClickManager__ctor_m20FBA23A5BC2D9B05DF27CB74CA1CFAD8ED7B70B (void);
+// 0x0000000D System.Void DropdownSelection::Update()
+extern void DropdownSelection_Update_m1F259B9882F64979EBE504302DB8C87D10966204 (void);
+// 0x0000000E System.Void DropdownSelection::HandleInputDate(System.Int32)
+extern void DropdownSelection_HandleInputDate_mA2C0CCFA3F6BA26446936B1805783B7BADDE8DF6 (void);
+// 0x0000000F System.Void DropdownSelection::.ctor()
+extern void DropdownSelection__ctor_m834716E38D133C22E7D7D49552C9938884F5B1A7 (void);
+// 0x00000010 System.Collections.IEnumerator GremlinTimers::FirstTimerCoro(System.Single)
+extern void GremlinTimers_FirstTimerCoro_mE5BBDE82C1D85F89DF08272AB128AED4CF25FCDC (void);
+// 0x00000011 System.Void GremlinTimers::Start()
+extern void GremlinTimers_Start_mD2B13D12B1FC4086B08D065316BAABBEAFA88D14 (void);
+// 0x00000012 System.Void GremlinTimers::.ctor()
+extern void GremlinTimers__ctor_m42750F85C248C65BD5B2205EFC656EDE789EF7FA (void);
+// 0x00000013 System.Void GremlinTimers/<FirstTimerCoro>d__7::.ctor(System.Int32)
+extern void U3CFirstTimerCoroU3Ed__7__ctor_m92892C371AA54F310015695FD66A6DE2A3470031 (void);
+// 0x00000014 System.Void GremlinTimers/<FirstTimerCoro>d__7::System.IDisposable.Dispose()
+extern void U3CFirstTimerCoroU3Ed__7_System_IDisposable_Dispose_m9D3B82CA589EA5845DD1E0C5F1C09103BF84C135 (void);
+// 0x00000015 System.Boolean GremlinTimers/<FirstTimerCoro>d__7::MoveNext()
+extern void U3CFirstTimerCoroU3Ed__7_MoveNext_mBCA4B0600A2EA05E8C23C8C6A2708D665E91EE36 (void);
+// 0x00000016 System.Object GremlinTimers/<FirstTimerCoro>d__7::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CFirstTimerCoroU3Ed__7_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mFEEB35962F6F76CCBB8875420DBA17059E858C75 (void);
+// 0x00000017 System.Void GremlinTimers/<FirstTimerCoro>d__7::System.Collections.IEnumerator.Reset()
+extern void U3CFirstTimerCoroU3Ed__7_System_Collections_IEnumerator_Reset_m23B6EB98872504F4E9B2C55440097C19FF3144F3 (void);
+// 0x00000018 System.Object GremlinTimers/<FirstTimerCoro>d__7::System.Collections.IEnumerator.get_Current()
+extern void U3CFirstTimerCoroU3Ed__7_System_Collections_IEnumerator_get_Current_mB13B9BB956A7C5634B95B8699ED01D4C6EB67F6C (void);
+// 0x00000019 System.Void OnOffLight::Start()
+extern void OnOffLight_Start_mB89C6ED2DA9033C2BF5BB612ACE42E100652F50F (void);
+// 0x0000001A System.Void OnOffLight::Update()
+extern void OnOffLight_Update_m0AE4D17B08AFFC03AF19160276F93CFD4E44C2C2 (void);
+// 0x0000001B System.Void OnOffLight::.ctor()
+extern void OnOffLight__ctor_m7731C11D10DA9FA98B022F8C6BFA10A7174DD230 (void);
+// 0x0000001C System.Void RandomiseOptions::Update()
+extern void RandomiseOptions_Update_m6897D89CB2CDF6BD87118C344A5D95810E5BC1F8 (void);
+// 0x0000001D System.Void RandomiseOptions::.ctor()
+extern void RandomiseOptions__ctor_m457AAB369A50B8703140887A6FCD1D7D0A241B7E (void);
+// 0x0000001E System.Void UniversalVariables::.cctor()
+extern void UniversalVariables__cctor_m6C36723BC020C2B0454F17B4AB75EB39D261E7B5 (void);
+static Il2CppMethodPointer s_methodPointers[30] = 
+{
+	OptionConsistency__ctor_m931190602B62B8B1733C892DAFCAA542A6E4BD0E,
+	NewBehaviourScript_Start_m783F84A617DADC4574B0BF1524481E6B96C65661,
+	NewBehaviourScript_Update_m411C4D5C2D993FD70092FDA0FE2AC4786F8AC001,
+	NewBehaviourScript__ctor_m437970EA37D66BDF32972F4CC0F65B95E5961FAA,
+	ChangeCheckBox_Update_m3FFF2668E216715A6B9F25780D2C97757941FE0C,
+	ChangeCheckBox__ctor_m8C1B96BC249441C39F6EED5A085A78F9CC8EB76B,
+	ChangeSwitchOnOff_Update_mC5D7F1D03BE42DFCA6EA95809AE8148E0023DDAE,
+	ChangeSwitchOnOff__ctor_m3FFDD1A009F7820404AC21FDECB605A7A427023B,
+	ClickManager_Awake_m8C1F7C71D1AA3E1CC537E6C385BC3DA4B88D046C,
+	ClickManager_Update_mEE9B6D5B06132A499849992D38ED889DCA287317,
+	ClickManager_SceneChange_m5DEACF16589656AED9DA6A240B68C5BE499B0FE8,
+	ClickManager__ctor_m20FBA23A5BC2D9B05DF27CB74CA1CFAD8ED7B70B,
+	DropdownSelection_Update_m1F259B9882F64979EBE504302DB8C87D10966204,
+	DropdownSelection_HandleInputDate_mA2C0CCFA3F6BA26446936B1805783B7BADDE8DF6,
+	DropdownSelection__ctor_m834716E38D133C22E7D7D49552C9938884F5B1A7,
+	GremlinTimers_FirstTimerCoro_mE5BBDE82C1D85F89DF08272AB128AED4CF25FCDC,
+	GremlinTimers_Start_mD2B13D12B1FC4086B08D065316BAABBEAFA88D14,
+	GremlinTimers__ctor_m42750F85C248C65BD5B2205EFC656EDE789EF7FA,
+	U3CFirstTimerCoroU3Ed__7__ctor_m92892C371AA54F310015695FD66A6DE2A3470031,
+	U3CFirstTimerCoroU3Ed__7_System_IDisposable_Dispose_m9D3B82CA589EA5845DD1E0C5F1C09103BF84C135,
+	U3CFirstTimerCoroU3Ed__7_MoveNext_mBCA4B0600A2EA05E8C23C8C6A2708D665E91EE36,
+	U3CFirstTimerCoroU3Ed__7_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mFEEB35962F6F76CCBB8875420DBA17059E858C75,
+	U3CFirstTimerCoroU3Ed__7_System_Collections_IEnumerator_Reset_m23B6EB98872504F4E9B2C55440097C19FF3144F3,
+	U3CFirstTimerCoroU3Ed__7_System_Collections_IEnumerator_get_Current_mB13B9BB956A7C5634B95B8699ED01D4C6EB67F6C,
+	OnOffLight_Start_mB89C6ED2DA9033C2BF5BB612ACE42E100652F50F,
+	OnOffLight_Update_m0AE4D17B08AFFC03AF19160276F93CFD4E44C2C2,
+	OnOffLight__ctor_m7731C11D10DA9FA98B022F8C6BFA10A7174DD230,
+	RandomiseOptions_Update_m6897D89CB2CDF6BD87118C344A5D95810E5BC1F8,
+	RandomiseOptions__ctor_m457AAB369A50B8703140887A6FCD1D7D0A241B7E,
+	UniversalVariables__cctor_m6C36723BC020C2B0454F17B4AB75EB39D261E7B5,
+};
+static const int32_t s_InvokerIndices[30] = 
+{
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	1263,
+	1509,
+	1509,
+	1253,
+	1509,
+	982,
+	1509,
+	1509,
+	1253,
+	1509,
+	1487,
+	1467,
+	1509,
+	1467,
+	1509,
+	1509,
+	1509,
+	1509,
+	1509,
+	2349,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	30,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
